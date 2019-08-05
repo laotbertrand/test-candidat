9f56bf8f7e2810e6749c4302d870d4a1a7d23477
Syl D <sdelescluse@atecna.fr>
2019-08-05 15:10:16 +0200

    feat (tool) : Copy assets/images

--- build/tasks/copy.js
+++ build/tasks/copy.js
@@ -1,10 +1,22 @@
 const fs = require('fs-extra');
 const colors = require('colors');
+const mkdirp = require('mkdirp');
 
 module.exports.copyIndex = () => {
 
   fs.copy('src/index.html', 'target/index.html')
     .then(() => console.log(`copy index.html success!`.bold))
     .catch(err => console.error(err));
+
+  mkdirp('target/images', error => {
+
+    if(error) {
+      throw error;
+    }
+
+    fs.copy('assets/images/', 'target/images/')
+      .then(() => console.log(`copy images success!`.bold))
+      .catch(err => console.error(err));
+  });
 
 }
